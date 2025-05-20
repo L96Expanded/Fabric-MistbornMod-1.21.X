48e406a1d7b23c6278938b793f614f2f9e1cb2c8
DaVeHe <davidvelascoherruzo@gmail.com>
2025-05-20 13:36:07 +0200

    Initial commit

---
+++ src/main/java/net/barope/mistbornmod/FallenAshes.java
@@ -0,0 +1,19 @@
+package net.barope.mistbornmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FallenAshes implements ModInitializer {
+	public static final String MOD_ID = "fallenashes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static class FallenAshesClient {
+	}
+}
---
+++ src/main/java/net/barope/mistbornmod/FallenAshesClient.java
@@ -0,0 +1,10 @@
+package net.barope.mistbornmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FallenAshesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
